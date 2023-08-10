f406a63e595c0cf550c8d1fe3c7dcfc0973ce032
Florencia <mfc.mdp86.gmail.com>
2023-08-10 18:58:18 -0300

    Ejercicios de JS

---
+++ script.js
@@ -0,0 +1,32 @@
+// Ejercicio: Lista de Súper - Parte I
+
+// let listaDeSuper = [];
+
+// listaDeSuper[0]= "yerba";
+// listaDeSuper[1]= "cafe";
+// listaDeSuper[2]= "agua";
+// listaDeSuper[3]= "vino";
+
+// listaDeSuper[0];
+// let ultimoElemento = listaDeSuper.length-1 ;
+
+// listaDeSuper[ultimoElemento];
+
+//Ejercicio Lista de Amigos
+
+// En este ejercicio deberás crear un Arreglo con 5 amigos. Dos de ellos, deben repetirse.
+// Luego:
+// Escribí un código que chequee si tenés algun amigo llamado Félix.
+// Si es así, deberá mostrar un mensaje que diga: "Tengo un amigo que se llama Félix", sino: "Sería bueno tener un amigo que se llame Félix".
+// Transformá tu código en una Función que reciba como Argumento cualquier nombre y verifique si tenés ese amigo o no (repitiendo las mismas frases que en el punto anterior).
+
+
+// let amigos = ["Micaela", "Soledad", "Melisa", "Melanie", "Soledad"];
+
+// function tengoAmigo (amigo) {
+// if (amigos.indexOf(amigo) >= 0) {
+//     console.log ("Tengo un amigo que se llama " + amigo);
+// } else {
+//     console.log ("Sería bueno tener un amigo que se llame " + amigo);
+// }
+// }
